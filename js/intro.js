12639bd147cfccba150594d651082bb1e3fe22e2
woollywood <Gerbert.Wells969@yandex.ru>
2023-10-30 00:19:33 +0300

    animation

--- js/intro.js
+++ js/intro.js
@@ -21,6 +21,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
 				document.body.classList.remove('loading');
 				intro.remove();
 				bodyLockToggle();
+
+				gsapAnimationInit();
 			}
 		});
 	}
