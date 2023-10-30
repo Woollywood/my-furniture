46c24d795b43dab1107400fcc2395ebf7855b09e
woollywood <Gerbert.Wells969@yandex.ru>
2023-10-30 22:48:17 +0300

    people fix

--- js/main.js
+++ js/main.js
@@ -1233,6 +1233,7 @@ function getHeaderHeight() {
 
 const mainSlider = new Splide('#main-slider', {
 	type: 'loop',
+	speed: 800,
 	perPage: 1,
 	autoplay: true,
 	classes: {
@@ -1241,6 +1242,21 @@ const mainSlider = new Splide('#main-slider', {
 		prev: 'splide__arrow--prev main-slider__arrow-prev',
 		next: 'splide__arrow--next main-slider__arrow-next',
 	},
+});
+
+const mainSliderList = document.querySelector('.main-slider__list');
+const mainSliderPeople = document.querySelector('.main-slider__people-bg');
+
+const sliderMovingObserver = new MutationObserver((e) => {
+	const style = mainSliderList.style.transform;
+	const styleValue = Number.parseFloat(style.slice(style.indexOf('(') + 1, -1));
+	mainSliderPeople.style.left = `calc(50% + ${-styleValue}px)`;
+});
+
+sliderMovingObserver.observe(mainSliderList, {
+	childList: true,
+	attributes: true,
+	attributeFilter: ['style'],
 });
 
 mainSlider.mount();
