a09b196860562f529ce368b0f97c3abbcfacdfaf
woollywood <Gerbert.Wells969@yandex.ru>
2023-11-02 20:00:59 +0300

    loader bugfix slow connection

--- js/intro.js
+++ js/intro.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+	console.log('intro start');
+	
 	const intro = document.querySelector('.intro');
 	bodyLockToggle();
 	scrollTo(0, 0);
 
 	const logo = document.querySelector('.intro__logo');
 	const spinnerText = document.querySelector('.intro__logo-mask');
+	spinnerText.classList.add('animation');
 	spinnerText.addEventListener('animationend', loaded);
 
 	function loaded(event) {
+		console.log('animation end');
+
 		const realLogo = document.querySelector('.header__logo');
 		const logoBox = realLogo.getBoundingClientRect();
 
@@ -18,12 +23,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
 			scale: 1,
 
 			onComplete: () => {
+				console.log('complete');
+
 				document.body.classList.remove('loading');
 				intro.remove();
 				bodyLockToggle();
 
 				gsapAnimationInit();
-			}
+			},
 		});
 	}
 });
